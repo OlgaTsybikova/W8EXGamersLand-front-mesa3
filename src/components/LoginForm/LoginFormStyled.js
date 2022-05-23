72f6df4324d985b0a7fa93313f64f8fd64de40f5
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-05-23 18:39:29 +0200

    fixed form styles

--- src/components/LoginForm/LoginFormStyled.js
+++ src/components/LoginForm/LoginFormStyled.js
@@ -5,7 +5,6 @@ const LoginFormContainer = styled.div`
   border: 2px solid rgb(243, 244, 246);
   padding: 3rem 2.5rem;
   width: 448px;
-  height: 512px;
   border-radius: 1.5rem;
   box-shadow: 0 20px 25px -5px rgba(0, 0, 0, 0.1),
     0 8px 10px -6px rgba(0, 0, 0, 0.1);
@@ -16,18 +15,17 @@ const LoginFormContainer = styled.div`
     }
 
     &__label {
-      font-weight: 500;
-      margin-bottom: 2rem;
-      font-size: 1.4rem;
+      font-weight: 400;
+      font-size: 1.3rem;
     }
 
     &__input {
-      position: relative;
-      width: 100%;
+      width: calc(100% - 2rem);
       border: 1px solid #222;
       border-radius: 0.375rem;
-      padding: 0.75rem 0;
+      padding: 0.75rem 1rem;
       outline: none;
+      font-size: 1rem;
     }
 
     &__button {
@@ -37,6 +35,17 @@ const LoginFormContainer = styled.div`
       font-weight: 500;
       font-size: 1rem;
       line-height: 1.25rem;
+      cursor: pointer;
+    }
+
+    &__text {
+      margin-top: 1rem;
+      display: flex;
+      justify-content: center;
+      font-size: 1rem;
+      font-weight: 400;
+      padding-left: 0.5rem;
+      padding-right: 0.5rem;
     }
   }
 `;
