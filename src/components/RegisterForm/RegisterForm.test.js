c558b24ed6f90d44b27068e0960752190e9a6679
Marian <marianlopig@gmail.com>
2022-05-23 23:44:47 +0200

    test register form and refactur from form

--- src/components/RegisterForm/RegisterForm.test.js
+++ src/components/RegisterForm/RegisterForm.test.js
@@ -1,7 +1,19 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { Provider } from "react-redux";
 import store from "../../redux/store/store";
+import { registerThunk } from "../../redux/thunks/userThunks";
 import RegisterForm from "./RegisterForm";
+
+const mockDispatch = jest.fn();
+jest.mock("react-redux", () => ({
+  ...jest.requireActual("react-redux"),
+  useDispatch: () => mockDispatch,
+}));
+
+jest.mock("../../redux/thunks/userThunks", () => ({
+  registerThunk: jest.fn(),
+}));
 
 describe("Given the RegisterForm component", () => {
   describe("When it's called", () => {
@@ -23,6 +35,34 @@ describe("Given the RegisterForm component", () => {
       expect(textInputs).toHaveLength(expectedTextInputs);
       expect(passwordInputs).toHaveLength(expectedPasswordInputs);
       expect(buttons).toHaveLength(expectedButtons);
+    });
+  });
+
+  describe("When the user click on submit button with all fields filled", () => {
+    test("Then it should dispatch register Thunk", () => {
+      render(
+        <Provider store={{ ...store }}>
+          <RegisterForm />
+        </Provider>
+      );
+
+      const expectedFormData = {
+        name: "Marian",
+        username: "marianlop",
+        password: "password",
+      };
+      const nameField = screen.getByPlaceholderText("Name");
+      const usernameField = screen.getByPlaceholderText("Username");
+      const passwordField = screen.getByPlaceholderText("Password");
+      const button = screen.getByRole("button");
+
+      userEvent.type(nameField, "Marian");
+      userEvent.type(passwordField, "password");
+      userEvent.type(usernameField, "marianlop");
+      userEvent.click(button);
+
+      expect(registerThunk).toHaveBeenCalledWith(expectedFormData);
+      expect(mockDispatch).toHaveBeenCalledWith(registerThunk());
     });
   });
 });
