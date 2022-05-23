c2f22afe7f8bbfc3c34ac2133b7c9cf5bb0d04b3
Marian <marianlopig@gmail.com>
2022-05-24 00:48:14 +0200

    test for login call submit done

--- src/components/LoginForm/LoginForm.test.js
+++ src/components/LoginForm/LoginForm.test.js
@@ -1,8 +1,20 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import { Provider } from "react-redux";
 import store from "../../redux/store/store";
+import { loginThunk } from "../../redux/thunks/userThunks";
 import LoginForm from "./LoginForm";
+
+const mockDispatch = jest.fn();
+jest.mock("react-redux", () => ({
+  ...jest.requireActual("react-redux"),
+  useDispatch: () => mockDispatch,
+}));
+
+jest.mock("../../redux/thunks/userThunks", () => ({
+  loginThunk: jest.fn(),
+}));
 
 describe("Given a LoginForm component", () => {
   describe("When it's rendered", () => {
@@ -24,6 +36,30 @@ describe("Given a LoginForm component", () => {
       expect(textInputs).toHaveLength(expectedTextInputs);
       expect(passwordInputs).toHaveLength(expectedPasswordInputs);
       expect(buttons).toHaveLength(expectedButtons);
+    });
+  });
+  describe("When the user click on submit button with all fields filled", () => {
+    test("Then it should dispatch login Thunk", () => {
+      render(
+        <Provider store={{ ...store }}>
+          <LoginForm />
+        </Provider>
+      );
+
+      const expectedFormData = {
+        username: "marianlop",
+        password: "password",
+      };
+      const usernameField = screen.getByLabelText("username");
+      const passwordField = screen.getByLabelText("password");
+      const button = screen.getByText("Log In");
+
+      userEvent.type(passwordField, "password");
+      userEvent.type(usernameField, "marianlop");
+      userEvent.click(button);
+
+      expect(loginThunk).toHaveBeenCalledWith(expectedFormData);
+      expect(mockDispatch).toHaveBeenCalledWith(loginThunk());
     });
   });
 });
