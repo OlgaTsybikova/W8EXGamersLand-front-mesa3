8be5858ea1540a60ae80d3cd38f237fd0d2326f9
Francesco Fabrissin <francescofabrissin.gmail.com>
2022-05-23 21:43:18 +0200

    added error handler in loginThunk

--- src/redux/thunks/userThunks.js
+++ src/redux/thunks/userThunks.js
@@ -8,14 +8,18 @@ import jwtDecode from "jwt-decode";
 export const loginThunk = (userData) => async (dispatch) => {
   const url = `${process.env.REACT_APP_API_URL}/users/login`;
 
-  const {
-    data: { token },
-  } = await axios.post(url, userData);
+  try {
+    const {
+      data: { token },
+    } = await axios.post(url, userData);
 
-  if (token) {
-    localStorage.setItem("token", token);
-    const { name, username } = jwtDecode(token);
-    dispatch(loginActionCreator({ name, username }));
+    if (token) {
+      localStorage.setItem("token", token);
+      const { name, username } = jwtDecode(token);
+      dispatch(loginActionCreator({ name, username }));
+    }
+  } catch (error) {
+    return error.message;
   }
 };
 
