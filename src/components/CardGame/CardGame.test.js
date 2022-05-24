eac967f0c30e2e8cbf3bd204cfdb0ebb3657e315
Francesco Fabrissin <francescofabrissin.gmail.com>
2022-05-24 21:25:38 +0200

    fixed thunk and card tests

--- src/components/CardGame/CardGame.test.js
+++ src/components/CardGame/CardGame.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import mockGames from "../../mocks/mockGames";
 import store from "../../redux/store/store";
 import CardGame from "./CardGame";
 
@@ -8,10 +9,11 @@ describe("Given a CardGame component", () => {
   describe("When its should the card whit 1 h2", () => {
     test("Then it should show the img its the same to 1", () => {
       const expectCard = 1;
+
       render(
         <Provider store={store}>
           <BrowserRouter>
-            <CardGame />
+            <CardGame game={mockGames[0]} />
           </BrowserRouter>
         </Provider>
       );
