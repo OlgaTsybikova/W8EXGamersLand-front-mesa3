6d9ab08a282c121c29d8fdf51178bfce6d382076
Marian <marianlopig@gmail.com>
2022-05-24 13:40:14 +0200

    changes in paths to solve navigate problems

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Navigate, Route, Routes } from "react-router-dom";
+import GamesPage from "./pages/GamesPage/GamesPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
 
@@ -7,9 +8,10 @@ function App() {
     <>
       <h1>GamersLand in process...</h1>
       <Routes>
-        <Route path="/" element={<Navigate to="/LoginPage" />} />
-        <Route path="/loginpage" element={<LoginPage />} />
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/games" element={<GamesPage />} />
       </Routes>
     </>
   );
