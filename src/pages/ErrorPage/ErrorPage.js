cba57930749dd42a5d0fc64e315fcd491cff8b4a
Francesco Fabrissin <francescofabrissin.gmail.com>
2022-05-24 23:05:34 +0200

    centered elements in error page

--- src/pages/ErrorPage/ErrorPage.js
+++ src/pages/ErrorPage/ErrorPage.js
@@ -1,9 +1,17 @@
+import styledComponents from "styled-components";
 const ErrorPage = () => {
+  const ErrorPageStyled = styledComponents.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  text-align: center;
+`;
+
   return (
-    <>
+    <ErrorPageStyled>
       <h1>ERROR 404</h1>
       <h2>PAGE NOT FOUND</h2>
-    </>
+    </ErrorPageStyled>
   );
 };
 
